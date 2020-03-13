f58cf8bffb0fadff3eddd1121a25b2220ace147f
khon0316 <kimhoon0316@gmail.com>
2020-03-13 21:27:25 +0900

    Front Nav css ing

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,6 +1,5 @@
 <template>
 <div>
-    HI
     <Header />
 </div>
 </template>
--- resources/js/components/layouts/Header.vue
+++ resources/js/components/layouts/Header.vue
@@ -1,7 +1,12 @@
 <template>
-<header>
+    <header class="flex items-center justify-between flex-wrap bg-white p-6">
+        <div class="flex items-center text-blue-100">Effort-Time</div>
 
-</header>
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <a>Login</a>
+            <a>Register</a>
+        </div>
+    </header>
 </template>
 
 <script>

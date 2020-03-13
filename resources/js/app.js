8380ab8e9e5934b287111cb80cde3fbaf1f62bd3
khon0316 <kimhoon0316@gmail.com>
2020-03-13 15:13:33 +0900

    Front Setting

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Vuex from 'vuex';
+import {routes} from './routes';
+import StoreData from './store';
+
+Vue.use(VueRouter);
+Vue.use(Vuex);
+
+const store = new Vuex.Store(StoreData);
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+});
+
+const app = new Vue({
+    el: '#app',
+    router,
+    store,
+    components: {}
+});
